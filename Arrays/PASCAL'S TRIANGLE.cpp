5a8e9981e36d4e00cc8d26d4c34e22d6687c5051
karthik <karthikveluru777@gmail.com>
2022-07-30 19:26:11 +0530

    new problem

---
+++ Arrays/PASCAL'S TRIANGLE.cpp
@@ -0,0 +1,30 @@
+/******************************************************************************
+-----------------------PASCAL'S TRIANGLE USING MATRIX------------
+
+*******************************************************************************/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{    int n;
+    cin>>n;
+    vector<vector<int>>v(n);
+    for(int i=0;i<n;i++)
+    {    v[i].resize(i+1);
+          v[i][0]=v[i][i]=1;
+        for(int j=1;j<i;j++)
+           v[i][j]=v[i-1][j-1]+v[i-1][j];
+    }
+    for(int i=0;i<n;i++)
+    {    for(int j=0;j<n-i-1;j++)
+           cout<<" ";
+        for(int j=0;j<v[i].size();j++)
+         cout<<v[i][j]<<" ";
+         cout<<endl;
+    }
+    
+
+    return 0;
+}
